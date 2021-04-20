869de0943f82a55325a14514c486c7ddd1d4c6c2
Ross Snider <rksnider7@gmail.com>
2021-04-20 09:29:17 -0600

    Update my_verification2.m
    Added header

--- intro/verification/example2/my_verification2.m
+++ intro/verification/example2/my_verification2.m
@@ -1,3 +1,26 @@
+%-------------------------------------------------------------------------
+% Description:  Matlab script that performs verification on 
+%               my_component2.vhd after it has been simulated using ModelSim
+%               running my_component2_tb.vhd and where simulation results
+%               have been written to output1.txt and output2.txt
+%-------------------------------------------------------------------------
+% Author:       Ross K. Snider
+% Company:      Montana State University
+% Create Date:  January 20, 2021
+% Revision:     1.0
+%-------------------------------------------------------------------------
+% Copyright (c) 2021 Ross K. Snider
+% All rights reserved. Redistribution and use in source and binary forms 
+% are permitted provided that the above copyright notice and this paragraph 
+% are duplicated in all such forms and that any documentation, advertising 
+% materials, and other materials related to such distribution and use 
+% acknowledge that the software was developed by Montana State University 
+% (MSU).  The name of MSU may not be used to endorse or promote products 
+% derived from this software without specific prior written permission.
+% THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+% IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+% WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+%-------------------------------------------------------------------------
 clear all  % clear all workspace variables
 close all  % close all figure windows
 
