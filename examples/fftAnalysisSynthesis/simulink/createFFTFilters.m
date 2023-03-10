a4bc647a4d78f76bfad250eeda67081ac7f4d4a7
Ross Snider <rksnider7@gmail.com>
2023-03-10 12:42:26 -0700

    Update createFFTFilters.m

--- examples/fftAnalysisSynthesis/simulink/createFFTFilters.m
+++ examples/fftAnalysisSynthesis/simulink/createFFTFilters.m
@@ -45,16 +45,16 @@ modelParams.highPassIndex      = ceil(modelParams.highPassCutoff / modelParams.a
 modelParams.fftGainsF3Real     = zeros(modelParams.fft.sizeHalf,1);  % start by zeroing array
 modelParams.fftGainsF3Real(modelParams.highPassIndex:end)  = ones(modelParams.fft.sizeHalf - modelParams.highPassIndex + 1, 1);
 modelParams.fftGainsF3Imag     = zeros(modelParams.fft.sizeHalf,1);  % zero imaginary part
-modelParams.fftGainsF1Real     = fi(modelParams.fftGainsF1Real,modelParams.fftROMDataType);
-modelParams.fftGainsF1Imag     = fi(modelParams.fftGainsF1Imag,modelParams.fftROMDataType);
+modelParams.fftGainsF3Real     = fi(modelParams.fftGainsF3Real,modelParams.fftROMDataType);
+modelParams.fftGainsF3Imag     = fi(modelParams.fftGainsF3Imag,modelParams.fftROMDataType);
 
 %----------------------------------------------------------------------------
 % Filter 4 - All Pass (filterSelect=3)
 %----------------------------------------------------------------------------
 modelParams.fftGainsF4Real     = ones(modelParams.fft.sizeHalf,1);
 modelParams.fftGainsF4Imag     = zeros(modelParams.fft.sizeHalf,1);  % zero imaginary part
-modelParams.fftGainsF1Real     = fi(modelParams.fftGainsF1Real,modelParams.fftROMDataType);
-modelParams.fftGainsF1Imag     = fi(modelParams.fftGainsF1Imag,modelParams.fftROMDataType);
+modelParams.fftGainsF4Real     = fi(modelParams.fftGainsF4Real,modelParams.fftROMDataType);
+modelParams.fftGainsF4Imag     = fi(modelParams.fftGainsF4Imag,modelParams.fftROMDataType);
 
 end
 
